7ec769eac56d91d7b628d2753a2a39972f7fb9f6
SirPanicSkill <txwnik@live.ru>
2018-03-17 18:40:38 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*globals jQuery, document */
+$(function() {
+    "use strict";
+
+   
+});
+
+/* video playing */
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+    function myFunction() {
+        if (video.paused) {
+            video.play();
+            btn.innerHTML = "Pause";
+        } else {
+            video.pause();
+            btn.innerHTML = "Play";
+        };
+    };
