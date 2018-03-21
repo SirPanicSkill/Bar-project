96fa7c552c0eefc4befb18b33d126ec8cc5b2256
SirPanicSkill <txwnik@live.ru>
2018-03-21 18:07:08 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
 /*globals jQuery, document */
 $(function() {
     "use strict";
-
-   
+    $(".button-collapse").sideNav();
+    //карусель
+    $('.carousel.carousel-slider').carousel({fullWidth: true, indicators: true});
+    //scrollFire
+    var options = [
+        {selector: '.fade1', offset: 200, callback: function(el) { Materialize.fadeInImage($(el)); } },
+        {selector: '.fade2', offset: 200, callback: function(el) { Materialize.fadeInImage($(el)); } },
+        {selector: '.fade3', offset: 200, callback: function(el) { Materialize.fadeInImage($(el)); } },
+    ];
+    Materialize.scrollFire(options);
+    //левое меню для телефонов
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is closed
+    }
+  );
 });
 
-/* video playing */
-// Get the video
-var video = document.getElementById("myVideo");
+/* после загрузки страницы */
+$(document).ready(function(){
+    //прелоадер
+    $('.progress').delay(250).fadeOut(1500);
 
-// Get the button
-var btn = document.getElementById("myBtn");
-
-// Pause and play the video, and change the button text
-    function myFunction() {
-        if (video.paused) {
-            video.play();
-            btn.innerHTML = "Pause";
-        } else {
-            video.pause();
-            btn.innerHTML = "Play";
-        };
-    };
+});
